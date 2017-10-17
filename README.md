629b55b4a332db6ce070a1b29a5af37e51a05c78
Gopinath Nelluri <gopinathnelluri@hotmail.com>
2017-10-17 18:49:38 -0400

    done.

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # BookList
 
 
-### Getting Started
-
-There are two methods for getting started with this repo.
 
 #### Want to try?
 Checkout this repo, install dependencies, then start the gulp process with the following:
--- src/components/app.js
+++ src/components/app.js
@@ -6,9 +6,9 @@ import BookDetail from "../containers/book-detail";
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div  >
       	<BookList/>
-      	<BookDetail/>
+      	<BookDetail />
       </div>
     );
   }
